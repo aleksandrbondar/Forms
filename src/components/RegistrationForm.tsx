0445bad6818d6de0cc0d4d4f994dc0c22fdcc1fc
aleksandrbondar <aleksandr_bondar@icloud.com>
2024-07-21 10:06:20 +0300

    v1.7

--- src/components/RegistrationForm.tsx
+++ src/components/RegistrationForm.tsx
@@ -3,13 +3,14 @@ import { Form, Formik } from "formik"
 import { Button } from "react-bootstrap"
 import { formConfig, FormValuesInterface, inputProps } from "../formConfig/FormConfig"
 import FormField from "./FormField"
+import Warning from "./FormComponents/Message/Warning"
 
 const RegistrationForm = () => {
   return (
     <Formik {...formConfig}>
       {({ isValid, isSubmitting, errors, touched }) => {
         return (
-          <Form className={`mb-3 p-3 rounded border ${(isValid || isSubmitting) ? ' border-success' : 'border-warning'}`} autoComplete="off">
+          <Form className={`mb-3 p-3 rounded border ${(isValid || isSubmitting) ? 'border-green-400' : 'border-warning'}`} autoComplete="off">
             {Array.from(inputProps.entries()).map(([name, props]) => {
               const inputProps = {
                 ...props,
@@ -20,8 +21,9 @@ const RegistrationForm = () => {
               return <FormField key={name} {...inputProps} />
             })}
             <Button variant={(isValid || isSubmitting) ? 'success' : 'warning'} type="submit" disabled={!isValid || isSubmitting}>
-              {(isValid || isSubmitting) ? 'Submit' : 'Enter all fields to submit'}
+              Sumbit
             </Button>
+            {!(isValid || isSubmitting) && <Warning warning="Enter all fields to submit" />}
           </Form>
         )
       }}
